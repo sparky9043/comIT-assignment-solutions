f40aa0eea8992af340a1015d74b34cc719299ec2
Se Joon Park <sparky9043@gmail.com>
2026-04-01 16:56:12 +0900

    feat: create Car class with basic constructor

---
+++ 03-30-2026/car.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, id: int, make: str, model: str, year: int,
+                 price: float, mileage: int):
+        # Your code here
+        self.id = id
+        self.make = make
+        self.model = model
+        self.year = year
+        self.price = price
+        self.mileage = mileage
