c99b6a1f45e4a9b103e65e67bcbe4f33a54e5a14
Se Joon Park <sparky9043@gmail.com>
2026-04-01 17:30:48 +0900

    refactor: create print_center and print_divider functions

--- 03-30-2026/main.py
+++ 03-30-2026/main.py
@@ -2,10 +2,34 @@ from car import Car
 from database import (initialize_database, import_cars, add_car, get_all_cars,
                         get_car_by_id, update_car, delete_car, search_cars)
 
+def print_center(text):
+    print(f"{text:^40}")
+
+def print_divider(shape="=", number=40):
+    print(shape * number)
+
 def show_menu():
-    print("\n" + "="*40)
-    print("   🚗  CAR DEALERSHIP MANAGER")
-    print("="*40)
+    print()
+    print_divider()
+    print_center(" 🚗 CAR DEALERSHIP MANAGER")
+    print_divider()
     # Print options 1–6 here
     # Return the user's input
-    pass
+    print_center("- MAIN MENU -")
+    print_center("1. Add a car")
+    print_center("2. Add a car")
+    print_divider()
+
+    choice = int(input("Enter your selection (1-6): "))
+    return choice
+
+def main():
+    # initialize_database()
+    # import_cars()
+    choice = show_menu()
+
+    cars = get_all_cars()
+    for car in cars:
+        print(car)
+
+main()
