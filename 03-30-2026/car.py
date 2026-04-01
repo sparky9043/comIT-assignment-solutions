03fac4660e59782ec02ccbf88d25cfaf2a1511db
Se Joon Park <sparky9043@gmail.com>
2026-04-01 17:09:51 +0900

    docs: add docstrings for the Car class and its methods

--- 03-30-2026/car.py
+++ 03-30-2026/car.py
@@ -1,7 +1,26 @@
 class Car:
+    """Represents a Car and its various properties
+
+    Attributes:
+        make (str): The make of the car
+        model (str): The model of the car
+        year (int): The manufacture year of the car
+        price (float): The unit price of the car
+        mileage (int): The mileage of the car
+        id (int): unique integer identifier for the car
+    """
     def __init__(self, make: str, model: str, year: int,
                  price: float, mileage: int, id = None):
-        # Your code here
+        """Initializes the car object with arguments
+
+        Args:
+            make (str): The make of the car
+            model (str): The model of the car
+            year (int): The manufacture year of the car; must be at least 0
+            price (float): The unit price of the car; must be at least 0
+            mileage (int): The mileage of the car; must be at least 0
+            id (int): unique integer identifier for the car; None by default
+        """
         if year < 0 or price < 0 or mileage < 0:
             raise ValueError('year, price and mileage must be at least 0')
 
@@ -17,6 +36,12 @@ class Car:
             f"${self.price:,.2f} | {self.mileage:,} km")
     
     def to_tuple(self):
+        """Converts the car object's make, model, year, price and mileage
+        to tuples
+
+        Returns:
+            tuple: An order tuple of the car's basic information
+        """
         return (self.make, self.model, self.year, self.price, self.mileage)
 
 # car = Car('Toyota', 'Camry', 2021, 24999.99, 15000, id=1)
