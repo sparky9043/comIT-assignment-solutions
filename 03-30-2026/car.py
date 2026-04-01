7721bd262a2ceafd1d29e2e96a29690e467f9623
Se Joon Park <sparky9043@gmail.com>
2026-04-01 17:01:32 +0900

    fix: make id default to None, move param to end and extend __str__

--- 03-30-2026/car.py
+++ 03-30-2026/car.py
@@ -1,10 +1,17 @@
 class Car:
-    def __init__(self, id: int, make: str, model: str, year: int,
-                 price: float, mileage: int):
+    def __init__(self, make: str, model: str, year: int,
+                 price: float, mileage: int, id = None):
         # Your code here
-        self.id = id
         self.make = make
         self.model = model
         self.year = year
         self.price = price
         self.mileage = mileage
+        self.id = id
+
+    def __str__(self):
+        return (f"[ID: {self.id}] {self.year} {self.make + self.model} | "
+            f"${self.price:,.2f} | {self.mileage:,} km")
+
+# car = Car('Toyota', 'Camry', 2021, 24999.99, 15000, id=1)
+# print(car)
