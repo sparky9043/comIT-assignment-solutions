efb5c1160de78af66b2b7cd86457095092e5b161
Se Joon Park <sparky9043@gmail.com>
2026-04-11 17:46:46 +0900

    feat: add print statements after each action

--- 04-02-2026/00_arrays.py
+++ 04-02-2026/00_arrays.py
@@ -4,17 +4,17 @@ fruits = ['apple', 'banana', 'grapes', 'tomato']
 
 # Appending to the end of the list: O(1)
 fruits.append('kiwi')
+print('Append to the end of list:', fruits)
 
 # Inserting to or deleting the beginning of the list: O(n) due to shift
 fruits.insert(0, 'mango')
+print('Insert to the beginning', fruits)
 fruits.remove('mango')
+print('Remove from the beginning', fruits)
 
 # Reading one element inside an array: O(1)
-print('Element at index 3',fruits[3])
+print('Reading element at index 3',fruits[3])
 
 # Reading all elements in the array: O(n)
-for fruit in fruits:
-    print(fruit)
-
-
-print(fruits)
+for index, fruit in enumerate(fruits):
+    print(f"Display index {index}", fruit)
