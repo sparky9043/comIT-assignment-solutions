f51995e5894fba48bd2468d46365e2bcbfcb1faf
Se Joon Park <sparky9043@gmail.com>
2026-04-11 18:22:22 +0900

    fix: change to index + 1 characters and O(1)

--- 04-02-2026/02_strings_basics.py
+++ 04-02-2026/02_strings_basics.py
@@ -6,7 +6,7 @@ chars = "inefficient example"
 for index, char in enumerate(chars):
     result += char
     print(f"Number of strings created count: {index + 1}", result)
-print(f"{index} strings created", result)
+print(f"{index + 1} strings created", result)
 
 # A more efficient way to rebuild strings: O(n)
 result = []
--- 04-02-2026/03_sets_usage.py
+++ 04-02-2026/03_sets_usage.py
@@ -8,7 +8,7 @@ numbers_set = set(numbers)
 
 print('Converting to sets (Displaying uniques only):', numbers_set)
 
-# Checking to see if an item exists: O(n)
+# Checking to see if an item exists: O(1)
 print('Is the number 2 in the list?', 2 in numbers_set)
 print('Is the number 1 in the list?', 1 in numbers_set)
 print('Is the number 3 in the list?', 3 in numbers_set)
