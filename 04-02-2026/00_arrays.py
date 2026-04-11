317dd5273cbaf825e43cf62d81b441d2b8d9d0c9
Se Joon Park <sparky9043@gmail.com>
2026-04-11 17:35:27 +0900

    feat: finish arrays example

---
+++ 04-02-2026/00_arrays.py
@@ -0,0 +1,16 @@
+# Example of list
+
+fruits = ['apple', 'banana', 'grapes', 'tomato']
+
+# Appending to the end of the list: O(1)
+fruits.append('kiwi')
+
+# Inserting to or deleting the beginning of the list: O(n) due to shift
+fruits.insert(0, 'mango')
+fruits.remove('mango')
+
+# Reading one element inside an array: O(1)
+print('Element at index 3',fruits[3])
+
+
+print(fruits)
