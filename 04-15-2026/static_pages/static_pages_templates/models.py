ee962c083ae48c6097689cc58a5fef460d38a779
Se Joon Park <sparky9043@gmail.com>
2026-04-16 11:34:46 +0900

    feat: create department and staff models

--- 04-15-2026/static_pages/static_pages_templates/models.py
+++ 04-15-2026/static_pages/static_pages_templates/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Staff(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
