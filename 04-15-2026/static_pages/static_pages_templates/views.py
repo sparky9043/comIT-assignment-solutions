f0ca05fdf233bd0594e0f6bc0b3305a55ad8cdde
Se Joon Park <sparky9043@gmail.com>
2026-04-16 11:44:32 +0900

    feat: render staff members and add theme to staff page

--- 04-15-2026/static_pages/static_pages_templates/views.py
+++ 04-15-2026/static_pages/static_pages_templates/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Department, Staff
 
 app_name = "static_pages_templates"
 
@@ -13,7 +14,9 @@ def index(request):
     return render(request, 'static_pages_templates/index.html', ctx)
 
 def staff(request):
-    return render(request, 'static_pages_templates/staff.html')
+    staff = Staff.objects.all()
+    context = { 'staff': staff }
+    return render(request, 'static_pages_templates/staff.html', context)
 
 def contact(request):
     return render(request, 'static_pages_templates/contact.html')
