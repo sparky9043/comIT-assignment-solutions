10d056a4ef283c4e70feeffec3e4fc2fae267d89
Se Joon Park <sparky9043@gmail.com>
2026-04-16 10:12:15 +0900

    feat: add boilerplate code for html and views and urls

--- 04-15-2026/static_pages/static_pages_templates/views.py
+++ 04-15-2026/static_pages/static_pages_templates/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+app_name = "static_pages_templates"
+
+def home(request):
+    ctx = {
+        'name': "Tony",
+        'age': 25,
+        'gains': 2344.322211
+    }
+    return render(request, 'static_pages_templates/home.html', ctx)
+
+def contact(request):
+    return render(request, 'static_pages_templates/contact.html')
