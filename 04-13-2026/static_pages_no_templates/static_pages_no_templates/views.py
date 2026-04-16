5b2f107ac3e647cf5a864fd5269e31d67fe92066
Se Joon Park <sparky9043@gmail.com>
2026-04-16 12:40:55 +0900

    feat: add 4 different views

--- 04-13-2026/static_pages_no_templates/static_pages_no_templates/views.py
+++ 04-13-2026/static_pages_no_templates/static_pages_no_templates/views.py
@@ -22,6 +22,21 @@ home_body = f"""
     
 """
 
+services_provided = [
+    "web development",
+    "react development",
+    "frontend",
+    "backend",
+    "UI/UX design"
+]
+
+members = {
+    "Tim Lee": 2,
+    "Jonathan Greenfield": 5,
+    "Quentin Larry": 7,
+    "Mario Lopez": 4,
+}
+
 def home(request):
     content= """
             <h1>Welcome to my Page</h1>
@@ -32,10 +47,27 @@ def home(request):
     return HttpResponse(nav + content + home_body)
 
 def contact(request):
-    return HttpResponse(nav + "Contact Us")
+    message = "Please contact us at"
+    number = "521-123-5231"
+    contact_info = f"<h3>{message}</h3>\n<h4>{number}</h4>"
+    div = f"<div>{contact_info}</div>"
+    
+    return HttpResponse(nav + div)
 
 def services(request):
-    return HttpResponse(nav + "Services")
+    string = ""
+    for service in services_provided:
+        string += f"<h3>{service}</h3>"
+        
+    div = f"<div>{string}</div>"
+    
+    return HttpResponse(nav + div)
 
 def staff(request):
-    return HttpResponse(nav + "Staff")
+    string = ""
+    for name, experience in members.items():
+        string += f"<h3>{name}</h3>\n<h3>Experience: {str(experience)} years</h3>"
+    
+    div = f"<div>{string}</div>"
+    
+    return HttpResponse(nav + div)
