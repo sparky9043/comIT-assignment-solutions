d8411af9e9a14e111622f37bc30d331be09dfe9f
Se Joon Park <sparky9043@gmail.com>
2026-04-17 10:31:12 +0900

    feat: update views spacing

--- 04-16-2026/auth_project/accounts/views.py
+++ 04-16-2026/auth_project/accounts/views.py
@@ -3,40 +3,46 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from  django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
 def landing(request):
     return render(request, 'landing.html')
 
-def login_view(request: HttpRequest) -> HttpResponse:
+
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        
         if user is not None:
             login(request, user)
             return redirect('dashboard')
         else:
-            messages.error(request, 'Invalid username or password')
+            messages.error(request, 'Invalid username or password.')
     return render(request, 'login.html')
 
-def register_view(request: HttpRequest) -> HttpResponse:
+
+def register_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
         if password != password2:
-            messages.error(request, 'Passwords do not match')
+            messages.error(request, 'Passwords do not match.')
         elif User.objects.filter(username=username).exists():
             messages.error(request, 'Username already taken.')
         else:
             User.objects.create_user(username=username, password=password)
-            messages.success(request, 'Account created. Please login')
-            return  redirect('login')
+            messages.success(request, 'Account created. Please log in.')
+            return redirect('login')
     return render(request, 'register.html')
 
+
 @login_required
-def dashboard(request: HttpRequest) -> HttpResponse:
+def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('landing')
