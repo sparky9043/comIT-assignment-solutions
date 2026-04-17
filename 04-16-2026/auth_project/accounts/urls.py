4ab9d1d6aa9030292315e0723635a822b63afcce
Se Joon Park <sparky9043@gmail.com>
2026-04-17 10:28:39 +0900

    feat: update code

--- 04-16-2026/auth_project/accounts/urls.py
+++ 04-16-2026/auth_project/accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing , name="landing"),
-    path('login/', views.login_view , name="login"),
-    path('register/', views.register_view , name="register"),
-    path('dashboard/', views.dashboard , name="dashboard"),
+    path('', views.landing, name='landing'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
 ]
