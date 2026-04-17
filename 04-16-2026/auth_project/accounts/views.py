7c74481a0f113e924704327ddc943a12e9d38076
Se Joon Park <sparky9043@gmail.com>
2026-04-17 10:07:53 +0900

    feat: add four views for login page

--- 04-16-2026/auth_project/accounts/views.py
+++ 04-16-2026/auth_project/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from  django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def landing(request):
+    return render(request, 'landing.html')
+
+def login_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+
+def register_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password != password2:
+            messages.error(request, 'Passwords do not match')
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already taken.')
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, 'Account created. Please login')
+            return  redirect('login')
+    return render(request, 'register.html')
+
+@login_required
+def dashboard_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'dashboard.html')
