7f6f0b18c2ccdd67da2411e74f24596ff8d0f393
Se Joon Park <sparky9043@gmail.com>
2026-04-24 13:51:13 +0900

    feat: create Animal model

--- 04-23-2026/zoo_app/animals/models.py
+++ 04-23-2026/zoo_app/animals/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(help_text="Age in years")
+    weight = models.FloatField(help_text="Weight in kilograms")
+    born_in_captivity = models.BooleanField(default=False)
+
+    date_added = models.DateField(default=timezone.now)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} (age {self.age})"
+
+    def get_absolute_url(self):
+        return reverse("animals:animal-detail", kwargs={"pk": self.pk})
