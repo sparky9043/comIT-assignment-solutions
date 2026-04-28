e47173336ca055eaf1518a0031af1ae255726454
Se Joon Park <sparky9043@gmail.com>
2026-04-28 11:07:11 +0900

    feat: make migrations with models

--- 04-27-2026/blog_project/blog/models.py
+++ 04-27-2026/blog_project/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
