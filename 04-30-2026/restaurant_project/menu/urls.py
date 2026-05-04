5906679afa685ce95079fe1c6efa2f983651f3df
Se Joon Park <sparky9043@gmail.com>
2026-05-04 12:12:52 +0900

    feat: show location detail and connect to url in location_list

--- 04-30-2026/restaurant_project/menu/urls.py
+++ 04-30-2026/restaurant_project/menu/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LocationList
+from .views import LocationList, LocationDetailView
 
 app_name = "menu"
 
 urlpatterns = [
     path("", LocationList.as_view(), name="location_list"),
+    path("location/<int:pk>", LocationDetailView.as_view(), name="location_detail"),
 ]
--- 04-30-2026/restaurant_project/menu/views.py
+++ 04-30-2026/restaurant_project/menu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Location
 
 # Create your views here.
@@ -9,3 +9,8 @@ class LocationList(ListView):
     model = Location
     context_object_name = "locations"
     template_name = "menu/location_list.html"
+
+
+class LocationDetailView(DetailView):
+    model = Location
+    template_name = "menu/location_detail.html"
