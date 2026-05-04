da6c0f6a15fa4d1935c2e065dbdb68113c7d9ecf
Se Joon Park <sparky9043@gmail.com>
2026-05-04 12:22:49 +0900

    build: show list of chefs and items at each location

--- 04-30-2026/restaurant_project/menu/views.py
+++ 04-30-2026/restaurant_project/menu/views.py
@@ -14,3 +14,9 @@ class LocationList(ListView):
 class LocationDetailView(DetailView):
     model = Location
     template_name = "menu/location_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["chefs"] = self.object.chefs.filter(is_staff=True)
+        context["items"] = self.object.menu_items.all()
+        return context
