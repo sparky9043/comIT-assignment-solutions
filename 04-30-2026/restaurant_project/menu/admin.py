f88e7c8c353d0aba985726e335d997904abb15d0
Se Joon Park <sparky9043@gmail.com>
2026-05-04 11:46:32 +0900

    chore: register models to admin site

--- 04-30-2026/restaurant_project/menu/admin.py
+++ 04-30-2026/restaurant_project/menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location, Chef, MenuItem
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Chef)
+admin.site.register(MenuItem)
