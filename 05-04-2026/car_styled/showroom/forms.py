2e3906dfb5a9632ea89f48729866e6328646c6d2
Se Joon Park <sparky9043@gmail.com>
2026-05-05 09:35:06 +0900

    feat: create Car and Seller forms

---
+++ 05-04-2026/car_styled/showroom/forms.py
@@ -0,0 +1,15 @@
+# showroom/forms.py
+from django import forms
+from .models import Car, Seller
+
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = ["make", "model", "year", "price", "transmission", "branch", "seller"]
+
+
+class SellerForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ["first_name", "last_name", "branches", "is_active"]
