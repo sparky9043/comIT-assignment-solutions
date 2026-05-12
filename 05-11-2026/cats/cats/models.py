7ce8626cae37d4f554194a7bcc6959e31f0f9db8
Se Joon Park <sparky9043@gmail.com>
2026-05-12 11:21:14 +0900

    feat: add remaining code for cats app

--- 05-11-2026/cats/cats/models.py
+++ 05-11-2026/cats/cats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=100)  # e.g., Persian, Siamese
+    age = models.IntegerField()
+    weight = models.FloatField()
+    vaccinated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- 05-11-2026/cats/cats/views.py
+++ 05-11-2026/cats/cats/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cat
+from .serializers import CatSerializer
 
-# Create your views here.
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_with = CatSerializer
+    serializer_class = CatSerializer
