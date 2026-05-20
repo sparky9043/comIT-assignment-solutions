9ab9cfda1c73c5fea7b1be5cde8fc99dff064ad8
Se Joon Park <sparky9043@gmail.com>
2026-05-20 14:15:14 +0900

    feat: create todo model

--- 05-14-2026/todoproject/todos/models.py
+++ 05-14-2026/todoproject/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]  # newest first
+
+    def __str__(self):
+        return self.title
